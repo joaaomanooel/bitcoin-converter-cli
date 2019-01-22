274b2931431d10a51844238d260bda419e214927
João Manoel Neto <joaaomanooel@gmail.com>
2019-01-22 15:00:23 -0200

    initial commit :tada:

---
+++ tests/main.spec.js
@@ -0,0 +1,14 @@
+const { expect } = require('chai');
+const { exec } = require('child_process');
+
+const btcConverter = './src/main.js';
+
+describe('Main CLI', () => {
+  it('Should return Hello World', (done) => {
+    exec(btcConverter, (err, stdout, stderr) => {
+      if (err) throw err;
+      expect(stdout.replace('\n', '')).to.be.equal('Hello World!');
+      done();
+    })
+  });
+})
