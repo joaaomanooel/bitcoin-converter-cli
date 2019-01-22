5ba24520cac7dc24bb0a82cd92713a1ec17e829a
João Manoel Neto <joaaomanooel@gmail.com>
2019-01-22 17:53:46 -0200

    add check version

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,13 +1,23 @@
+/* eslint-disable no-unused-expressions */
 const { expect } = require('chai');
 const { exec } = require('child_process');
+const pkg = require('../package.json');
 
 const btcConverter = './src/main.js';
 
 describe('Main CLI', () => {
-  it('Should return Hello World', (done) => {
-    exec(btcConverter, (err, stdout, stderr) => {
+  it('Should return version of bitcoin-converter', (done) => {
+    exec(`${btcConverter} --version`, (err, stdout, stderr) => {
       if (err) throw err;
-      expect(stdout.replace('\n', '')).to.be.equal('Hello World!');
+      expect(stdout.replace('\n', '')).to.be.equal(pkg.version);
+      done();
+    });
+  });
+
+  it('Should return the description when bitcoin-converter --help', (done) => {
+    exec(`${btcConverter} --help`, (err, stdout, stderr) => {
+      if (err) throw err;
+      expect(stdout.includes('Convert Bitcoin to any currency defined')).to.be.true;
       done();
     });
   });
