749c0eb001d5372039e7bbcd014c53bd613ea5b9
João Manoel Neto <joaaomanooel@gmail.com>
2019-02-06 14:43:13 -0200

    add loader

--- src/convertBTC.js
+++ src/convertBTC.js
@@ -1,11 +1,20 @@
 const chalk = require('chalk');
 const request = require('request');
+const ora = require('ora');
+
+const spinner = ora({
+  text: 'Retrieving Bitcoin data...',
+  color: 'yellow',
+});
 
 module.exports = (currency = 'USD', amount = 1) => {
   const url = `https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=${currency}&amount=${amount}`;
+  spinner.start();
 
   request(url, (error, response, body) => {
     let apiResponse;
+    spinner.stop();
+
     try {
       apiResponse = JSON.parse(body);
     } catch (parseError) {
@@ -13,6 +22,8 @@ module.exports = (currency = 'USD', amount = 1) => {
       return parseError;
     }
 
-    return console.log(`${chalk.red(amount)} BTC to ${chalk.cyan(currency)} = ${chalk.yellow(apiResponse.price)}`);
+    return console.log(
+      `${chalk.red(amount)} BTC to ${chalk.cyan(currency)} = ${chalk.yellow(apiResponse.price)}`,
+    );
   });
 };
