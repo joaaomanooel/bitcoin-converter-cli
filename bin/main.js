59eb18dd3d7108f740cd0a3c0182f0a1ecf2af35
João Manoel Neto <joaaomanooel@gmail.com>
2019-02-12 10:49:03 -0200

    fix commander problem

--- bin/main.js
+++ bin/main.js
@@ -7,5 +7,5 @@ var pkg = require('../package.json');
 
 var convertBRT = require('./convertBTC');
 
-program.version(pkg.version).description('Convert Bitcoin to any currency defined').option('-C, --currency <currency>', 'Curency to be convert. (Default: USD').option('-A, --amount <amount>', 'Value in Bitcoin to be convert. (Default: 1').parse(process.argv);
+program.version(pkg.version).description('Convert Bitcoin to any currency defined').option('-C, --currency <currency>', 'Curency to be convert. (Default: USD)').option('-A, --amount <amount>', 'Value in Bitcoin to be convert. (Default: 1)').parse(process.argv);
 convertBRT(program.currency, program.amount);
